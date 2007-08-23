637b778c00c45d98b0659134bd0492e602a7d0f1
smironov <smironov@17a1adf3-2038-0410-92b8-11115a6f368b>
2007-08-23 07:02:08 +0000

    Added finals

--- trunk/src/Unpack200.java
+++ trunk/src/Unpack200.java
@@ -1,10 +1,17 @@
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Pack200;
 
 public class Unpack200 {
-    public static void main(String[] args) throws IOException {
-        Pack200.Unpacker unpacker = Pack200.newUnpacker();
+    public static void main(final String[] args) throws IOException {
+        final Pack200.Unpacker unpacker = Pack200.newUnpacker();
         final String packFilename;
         final String jarFilename;
         if (args.length == 1) {
@@ -20,8 +27,8 @@ public class Unpack200 {
             System.exit(1);
             return;
         }
-        File packFile = new File(packFilename);
-        InputStream in = new BufferedInputStream(new FileInputStream(packFile));
+        final File packFile = new File(packFilename);
+        final InputStream in = new BufferedInputStream(new FileInputStream(packFile));
         final OutputStream out = new BufferedOutputStream(new FileOutputStream(jarFilename));
         try {
             final JarOutputStream jarOutputStream = new JarOutputStream(out);
